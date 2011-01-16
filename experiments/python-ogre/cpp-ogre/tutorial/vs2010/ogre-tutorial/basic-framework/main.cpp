030b1a74cbc3aa021148ffd7afcc2b87b1cd57f6
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-01-16 21:30:39 +0100

    Render to texture.
    Added a tutorial which uses Render-to-texture technique to show the rendered scene on a rectangle. Additionally the texture is inverted using a shader.

--- experiments/python-ogre/cpp-ogre/tutorial/vs2010/ogre-tutorial/basic-framework/main.cpp
+++ experiments/python-ogre/cpp-ogre/tutorial/vs2010/ogre-tutorial/basic-framework/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-#include "FullscreenQuad.h"
+#include "IntermediateTutorial7.h"
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -18,7 +18,7 @@ extern "C" {
 #endif
     {
         // Create application object
-        FullscreenQuad app;
+        IntermediateTutorial7 app;
 
         try {
             app.go();
