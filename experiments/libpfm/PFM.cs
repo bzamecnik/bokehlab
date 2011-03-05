3fc1cc9daab545c8ca7ddb34438d6c9e4e1fdafc
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-03-05 20:58:09 +0100

    Implement saving a PFM image. Fix reading PFM. Add a PFM I/O test.

--- experiments/libpfm/PFM.cs
+++ experiments/libpfm/PFM.cs
@@ -16,36 +16,40 @@ namespace libpfm
         public PixelFormat PixelFormat { get; private set; }
         public float Scale { get; private set; }
 
-        private uint ChannelsCount { get; set; }
+        public uint ChannelsCount { get; set; }
         public Endianness Endianness { get; private set; }
+
+        private static readonly int FLOAT_BYTE_COUNT = 4;
+        private static readonly char[] WHITESPACE = new char[] { '\n', '\r', ' ', '\t', '\f' };
 
         public PFMImage(uint width, uint height) :
             this(width, height, PixelFormat.RGB)
         { }
 
         public PFMImage(uint width, uint height, PixelFormat pixelFormat) :
-            this(width, height, pixelFormat, 1.0f)
+            this(width, height, pixelFormat, 1.0f, Endianness.LittleEndian)
         { }
 
-        public PFMImage(uint width, uint height, PixelFormat pixelFormat, float scale)
+        public PFMImage(uint width, uint height, PixelFormat pixelFormat, float scale, Endianness endianness)
         {
             Width = width;
             Height = height;
             PixelFormat = pixelFormat;
             ChannelsCount = GetChannelsCount(pixelFormat);
             Scale = scale;
+            Endianness = endianness;
             Image = new float[Width, Height, ChannelsCount];
         }
 
-        public static PFMImage LoadImage(string filename) {
+        public static PFMImage LoadImage(string filename)
+        {
             FileStream fs = new FileStream(filename, FileMode.Open);
 
-            // read header
+            // read the header
             PFMImage image = ReadHeader(fs);
 
             // read the image data
-            const int floatByteCount = 4;
-            byte[] rawIntensity = new byte[floatByteCount];
+            byte[] rawIntensity = new byte[FLOAT_BYTE_COUNT];
             for (int y = 0; y < image.Height; y++)
             {
                 for (int x = 0; x < image.Width; x++)
@@ -53,20 +57,15 @@ namespace libpfm
                     for (int band = 0; band < image.ChannelsCount; band++)
                     {
                         // read one float
-                        if (fs.Read(rawIntensity, 0, floatByteCount) < 0)
+                        if (fs.Read(rawIntensity, 0, FLOAT_BYTE_COUNT) != FLOAT_BYTE_COUNT)
                         {
-                            throw new OutOfMemoryException();
+                            throw new OutOfMemoryException(String.Format(
+                                "Cannot read float at: [{0}, {1}], channel {2}", x, y, band));
                         }
                         if (image.Endianness == Endianness.BigEndian)
                         {
                             // swap the bytes to little endian
-                            byte tmp = rawIntensity[0];
-                            rawIntensity[0] = rawIntensity[3];
-                            rawIntensity[3] = tmp;
-
-                            tmp = rawIntensity[1];
-                            rawIntensity[1] = rawIntensity[2];
-                            rawIntensity[2] = tmp;
+                            SwapFloatEndianness(ref rawIntensity);
                         }
                         float intensity = BitConverter.ToSingle(rawIntensity, 0);
                         image.Image[x, y, band] = intensity;
@@ -82,18 +81,13 @@ namespace libpfm
         private static PFMImage ReadHeader(FileStream fs)
         {
             // read signature - pixel format
-            StreamReader reader = new StreamReader(fs);
-            string line = reader.ReadLine();
-            if (line.Length < 2)
-            {
-                throw new OutOfMemoryException("Header is missing.");
-            }
-            PixelFormat pixelFormat;
-            if (line[0] != 'P')
+            string token = ReadToken(fs);
+            if ((token.Length != 2) || (token[0] != 'P'))
             {
                 throw new OutOfMemoryException("Bad header: image signature.");
             }
-            switch (line[1])
+            PixelFormat pixelFormat;
+            switch (token[1])
             {
                 case 'F':
                     pixelFormat = PixelFormat.RGB;
@@ -107,30 +101,26 @@ namespace libpfm
 
             // image dimensions - width, height
 
-            line = reader.ReadLine();
+            token = ReadToken(fs);
 
-            string[] dimensions = line.Split(new char[] { ' ', '\t', '\n', '\f' }, 2);
-            if ((dimensions == null) || (dimensions.Length != 2))
-            {
-                throw new OutOfMemoryException("Bad header: image dimensions");
-            }
             uint width;
-            if (!uint.TryParse(dimensions[0], out width))
+            if (!uint.TryParse(token, out width))
             {
                 throw new OutOfMemoryException("Bad header: image width");
             }
+            token = ReadToken(fs);
             uint height;
-            if (!uint.TryParse(dimensions[1], out height))
+            if (!uint.TryParse(token, out height))
             {
                 throw new OutOfMemoryException("Bad header: image height");
             }
 
             // image endianness and scale
-            line = reader.ReadLine();
+            token = ReadToken(fs);
             //reader.Dispose();
 
             float scale;
-            if (!float.TryParse(line, NumberStyles.Float, CultureInfo.CreateSpecificCulture("en-US"), out scale))
+            if (!float.TryParse(token, NumberStyles.Float, CultureInfo.CreateSpecificCulture("en-US"), out scale))
             {
                 throw new OutOfMemoryException("Bad header: image endianness and scale");
             }
@@ -138,17 +128,117 @@ namespace libpfm
             Endianness endianness = (scale < 0) ? Endianness.LittleEndian : Endianness.BigEndian;
             scale = Math.Abs(scale);
 
-            return new PFMImage(width, height, pixelFormat, scale);
+            return new PFMImage(width, height, pixelFormat, scale, endianness);
         }
 
-        public static void SaveImage(string filename, PFMImage image, Endianness endianness)
+        private static string ReadToken(FileStream fs)
         {
+            int currentByte;
+            StringBuilder token = new StringBuilder();
+            while (true) {
+                currentByte = fs.ReadByte();
+                if (currentByte < 0)
+                {
+                    //throw new OutOfMemoryException("Premature end of stream.");
+                    return token.ToString();
+                }
+                bool isWhitespace = WHITESPACE.Contains((char)currentByte);
+                if (isWhitespace)
+                {
+                    return token.ToString();
+                }
+                else
+                {
+                    token.Append((char)currentByte);
+                }
+            }
+        }
 
+        public void SaveImage(string filename)
+        {
+            SaveImage(filename, Endianness);
+        }
+
+        public void SaveImage(string filename, Endianness endianness)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Create);
+
+            // write the header
+            SaveHeader(fs, this);
+
+            // write the image data
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    for (int band = 0; band < ChannelsCount; band++)
+                    {
+                        float intensity = Image[x, y, band];
+                        byte[] rawIntensity = BitConverter.GetBytes(intensity);
+                        if (endianness == Endianness.BigEndian)
+                        {
+                            // swap the bytes from little to big endian
+                            SwapFloatEndianness(ref rawIntensity);
+                        }
+                        // write one float
+                        fs.Write(rawIntensity, 0, rawIntensity.Length);
+                    }
+                }
+            }
+
+            fs.Flush();
+            fs.Close();
+        }
+
+        private static void SaveHeader(FileStream fs, PFMImage image)
+        {
+            StreamWriter writer = new StreamWriter(fs);
+
+            // write PFM signature with information on the number of color channels
+            // RGB, greyscale
+            string signature;
+            switch (image.PixelFormat)
+            {
+                case PixelFormat.RGB:
+                    signature = "PF";
+                    break;
+                case PixelFormat.Greyscale:
+                    signature = "Pf";
+                    break;
+                default:
+                    throw new ArgumentException(String.Format("Unsupported pixel format: {0}", image.PixelFormat));
+            }
+            writer.Write("{0}\n", signature);
+
+            // write image dimensions - width, height
+            writer.Write("{0} {1}\n", image.Width, image.Height);
+
+            // write image scale and endianness
+            float scale = image.Scale;
+            if (image.Endianness == Endianness.LittleEndian)
+            {
+                scale = -scale;
+            }
+            writer.Write(String.Format(CultureInfo.CreateSpecificCulture("en-US"), "{0}\n", scale));
+
+            writer.Flush();
+        }
+
+        private static void SwapFloatEndianness(ref byte[] rawIntensity)
+        {
+            byte tmp = rawIntensity[0];
+            rawIntensity[0] = rawIntensity[3];
+            rawIntensity[3] = tmp;
+
+            tmp = rawIntensity[1];
+            rawIntensity[1] = rawIntensity[2];
+            rawIntensity[2] = tmp;
         }
 
         private static uint GetChannelsCount(PixelFormat pixelFormat)
         {
-            switch (pixelFormat) {
+            switch (pixelFormat)
+            {
                 case PixelFormat.RGB:
                     return 3;
                 case PixelFormat.Greyscale:
@@ -157,8 +247,6 @@ namespace libpfm
                     throw new ArgumentException(String.Format("Unsupported pixel format: {0}", pixelFormat));
             }
         }
-
-        
     }
 
     public enum PixelFormat
--- experiments/pfmUtil/Program.cs
+++ experiments/pfmUtil/Program.cs
@@ -2,19 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 using libpfm;
 
 namespace pfmUtil
 {
     class Program
     {
-        public static void DisplayInfo(string filename) {
-            PFMImage image = PFMImage.LoadImage(filename);
+        public static void DisplayInfo(PFMImage image) {
             Console.WriteLine("Portable float-map");
             Console.WriteLine("Pixel format: {0}", image.PixelFormat);
             Console.WriteLine("Width: {0}, height: {1}", image.Width, image.Height);
             Console.WriteLine("Scale: {0}", image.Scale);
             Console.WriteLine("Endianness: {0}", image.Endianness);
+        }
+
+        public static PFMImage CreateTestImage()
+        {
+            PFMImage image = new PFMImage(5, 7, PixelFormat.Greyscale);
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    image.Image[x, y, 0] = (x + 1) + (10 * y) + 0.5f;
+                }
+            }
+            return image;
+        }
+
+        private static void PrintImageContents(PFMImage image)
+        {
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Console.Write("[");
+                    for (int band = 0; band < image.ChannelsCount; band++)
+                    {
+                        Console.Write("{0}", String.Format(CultureInfo.CreateSpecificCulture("en-US"), "{0}", image.Image[x, y, band]));
+                        if (band < image.ChannelsCount - 1)
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                    Console.Write("]");
+                    if (x < image.Width - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
 
         static void Main(string[] args)
@@ -26,7 +64,23 @@ namespace pfmUtil
 
             try
             {
-                DisplayInfo(args[0]);
+                //PFMImage image = PFMImage.LoadImage(args[0]);
+                //DisplayInfo(image);
+                //string newImageFileName = filename + ".out";
+                //image.SaveImage(newImageFileName);
+                //DisplayInfo(PFMImage.LoadImage(newImageFileName));
+
+                Console.WriteLine("Writing test image.");
+                PFMImage image = CreateTestImage();
+                DisplayInfo(image);
+                PrintImageContents(image);
+                image.SaveImage(args[0]);
+                Console.WriteLine();
+
+                Console.WriteLine("Reading test image.");
+                PFMImage loadedImage = PFMImage.LoadImage(args[0]);
+                DisplayInfo(loadedImage);
+                PrintImageContents(loadedImage);
             }
             catch (OutOfMemoryException ex)
             {
