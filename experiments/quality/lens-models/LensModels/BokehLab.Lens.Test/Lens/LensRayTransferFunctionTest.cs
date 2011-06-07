d0bcd6ad57edf193bc18b32f8fcc1ebb4a61d8cd
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-06-07 17:12:53 +0200

    LensModels: Create a simple sampler for LRTF.
    Not complete already. Conversion outgoing rays to parameters is missing.

---
+++ experiments/quality/lens-models/LensModels/BokehLab.Lens.Test/Lens/LensRayTransferFunctionTest.cs
@@ -0,0 +1,27 @@
+﻿namespace BokehLab.RayTracing.Lens.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BokehLab.Math;
+    using BokehLab.RayTracing.Lens;
+    using OpenTK;
+    using Xunit;
+
+    class LensRayTransferFunctionTest
+    {
+        [Fact]
+        public void SampleLrtf()
+        {
+            ComplexLens lens = ComplexLens.CreateDoubleGaussLens(Materials.Fixed.AIR, 4.0);
+            LensRayTransferFunction lrtf = new LensRayTransferFunction(lens);
+            var table = lrtf.SampleLrtf(new Vector2d(0.5, 0.0), new Vector2d(1.0, 0.0),
+                LensRayTransferFunction.VariableParameter.DirectionPhi, 20);
+            foreach (Ray ray in table)
+            {
+                Console.WriteLine(ray);
+            }
+        }
+    }
+}
---
+++ experiments/quality/lens-models/LensModels/BokehLab.RayTracing/Lens/LensRayTransferFunction.cs
@@ -0,0 +1,128 @@
+﻿namespace BokehLab.RayTracing.Lens
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BokehLab.Math;
+    using BokehLab.RayTracing;
+    using OpenTK;
+
+    /// <summary>
+    /// Represents the Lens Ray Transfer Function (LRTF).
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The LRTF describes the behavior of a lens. It is a 4D->4D mapping from
+    /// incoming ray to outgoing rays. Each ray can be described by four
+    /// parameters: hemispherical coordinates (theta, phi) of the ray origin on
+    /// the back or front surface of the lens and hemispherical coordinates of
+    /// the ray direction (in a frame local to the ray origin with hemisphere
+    /// pole aligned with the surface normal - pointing outside the lens).
+    /// </para>
+    /// <para>
+    /// The purpose of the LRTF is to compute the ray traversal through a
+    /// complex lens more efficiently. Instead of permanently traversing the
+    /// lens its LRTF can be precomputed for some sample points and then
+    /// approximately evaluated with interpolation. The precomputed samples
+    /// can be stored in a table.
+    /// </para>
+    /// <para>
+    /// Note that the LRTF is not always defined - some incoming rays
+    /// can be absorbed inside the lens. Also rays that happen to be totally
+    /// internally reflected are considered as not leaving the lens.
+    /// </para>
+    /// <para>
+    /// For lenses with rotation symmetry around the optical axis the LRTF is
+    /// also rotationally symmetric and one of the input dimensions (position
+    /// phi) might not be stored. It is only needed to separate the rotation.
+    /// Then the evaluation consists of three phases:
+    /// - rotate the input (position and direction) so that its position phi
+    ///   is 0
+    /// - evaluate the LRTF
+    /// - rotate the ouput using the original position phi
+    /// </para>
+    /// </remarks>
+    public class LensRayTransferFunction
+    {
+        ComplexLens lens;
+
+        public LensRayTransferFunction(ComplexLens lens)
+        {
+            this.lens = lens;
+        }
+
+        /// <summary>
+        /// Sample the LRTF with fixed position and direction phi parameters,
+        /// ie. vary the respective theta parameters.
+        /// </summary>
+        /// <param name="sampleCount">Number of sample in each of the two
+        /// variable dimensions.</param>
+        /// <returns>Table of </returns>
+        public Ray[] SampleLrtf(Vector2d position, Vector2d direction,
+            VariableParameter variableParam, int sampleCount)
+        {
+            Ray[] table = new Ray[sampleCount];
+            double positionTheta = position.X;
+            double positionPhi = position.Y;
+            double directionTheta = direction.X;
+            double directionPhi = direction.Y;
+
+            switch (variableParam)
+            {
+                case VariableParameter.PositionTheta:
+                    positionTheta = 0;
+                    break;
+                case VariableParameter.PositionPhi:
+                    positionPhi = 0;
+                    break;
+                case VariableParameter.DirectionTheta:
+                    directionTheta = 0;
+                    break;
+                case VariableParameter.DirectionPhi:
+                    directionPhi = 0;
+                    break;
+                default:
+                    break;
+            }
+
+            double param = 0.0;
+            double step = 1 / (double)(sampleCount - 1);
+            for (int i = 0; i < sampleCount; i++)
+            {
+                switch (variableParam)
+                {
+                    case VariableParameter.PositionTheta:
+                        positionTheta += param;
+                        break;
+                    case VariableParameter.PositionPhi:
+                        positionPhi += param;
+                        break;
+                    case VariableParameter.DirectionTheta:
+                        directionTheta += param;
+                        break;
+                    case VariableParameter.DirectionPhi:
+                        directionPhi += param;
+                        break;
+                    default:
+                        break;
+                }
+                Ray incomingRay = lens.GetBackSurfaceRayFromParameters(
+                    new Vector2d(positionTheta, positionPhi),
+                    new Vector2d(directionTheta, directionPhi));
+                Ray outgoingRay = lens.Transfer(incomingRay);
+                table[i] = outgoingRay;
+                param += step;
+            }
+            return table;
+        }
+
+        public enum VariableParameter
+        {
+            PositionTheta,
+            PositionPhi,
+            DirectionTheta,
+            DirectionPhi,
+        }
+    }
+}
