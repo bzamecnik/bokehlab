d3c3de1de5d487773f4bb9c077a9fcc544d929ab
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2011-11-29 13:24:37 +0100

    Fix a bug where samples in a tile were correlated.

--- BokehLab/BokehLab.Math/Sampler.cs
+++ BokehLab/BokehLab.Math/Sampler.cs
@@ -383,14 +383,13 @@
         }
 
         //http://stackoverflow.com/questions/273313/randomize-a-listt-in-c-sharp
-        public static void Shuffle<T>(IList<T> list)
+        public void Shuffle<T>(IList<T> list)
         {
-            Random rng = new Random();
             int n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = rng.Next(n + 1);
+                int k = random.Next(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
